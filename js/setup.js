eeb5ad81445751b3484e2aa3516e1d58da38c1c2
ivanzolotov <ivanzolotov@ivanzolotov.ru>
2017-08-30 13:52:50 +0500

    Исправил опечатку в названии функции getRandomElement

--- js/setup.js
+++ js/setup.js
@@ -106,7 +106,7 @@ var setupWizardElt = setupElt.querySelector('.setup-wizard');
 var wizardCoatElt = setupWizardElt.querySelector('.wizard-coat');
 var inputCoatColorElt = setupElt.querySelector('input[name=coat-color]');
 wizardCoatElt.addEventListener('click', function (evt) {
-  var color = getRandonElement(WIZARD_COAT_COLORS);
+  var color = getRandomElement(WIZARD_COAT_COLORS);
   evt.currentTarget.style.fill = color;
   inputCoatColorElt.value = color;
 });
@@ -114,7 +114,7 @@ wizardCoatElt.addEventListener('click', function (evt) {
 var wizardEyesElt = setupWizardElt.querySelector('.wizard-eyes');
 var inputEyesColorElt = setupElt.querySelector('input[name=eyes-color]');
 wizardEyesElt.addEventListener('click', function (evt) {
-  var color = getRandonElement(WIZARD_EYES_COLORS);
+  var color = getRandomElement(WIZARD_EYES_COLORS);
   evt.currentTarget.style.fill = color;
   inputEyesColorElt.value = color;
 });
@@ -122,16 +122,16 @@ wizardEyesElt.addEventListener('click', function (evt) {
 var setupFireballWrapElt = setupElt.querySelector('.setup-fireball-wrap');
 var inputFireballColorElt = setupElt.querySelector('input[name=fireball-color]');
 setupFireballWrapElt.addEventListener('click', function (evt) {
-  var color = getRandonElement(FIREBALL_COLORS);
+  var color = getRandomElement(FIREBALL_COLORS);
   evt.currentTarget.style['background-color'] = color;
   inputFireballColorElt.value = color;
 });
 
 function makeRandomWizard() {
   return {
-    name: getRandonElement(WIZARD_FIRST_NAMES) + ' ' + getRandonElement(WIZARD_LAST_NAMES),
-    coatColor: getRandonElement(WIZARD_COAT_COLORS),
-    eyesColor: getRandonElement(WIZARD_EYES_COLORS),
+    name: getRandomElement(WIZARD_FIRST_NAMES) + ' ' + getRandomElement(WIZARD_LAST_NAMES),
+    coatColor: getRandomElement(WIZARD_COAT_COLORS),
+    eyesColor: getRandomElement(WIZARD_EYES_COLORS),
   };
 }
 
@@ -143,6 +143,6 @@ function makeWizardElement(data, template) {
   return wizardElement;
 }
 
-function getRandonElement(array) {
+function getRandomElement(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
